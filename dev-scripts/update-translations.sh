347353544d564e598ae5361ab2eb7208cb651009
Naqua Darazaki <n.darazaki@gmail.com>
2023-06-03 20:44:29 +0200

    Improve dev-scripts/update-translations.sh

--- dev-scripts/update-translations.sh
+++ dev-scripts/update-translations.sh
@@ -4,6 +4,7 @@ echo '/!\ Make sure that po/_base.pot has been updated with the new translations
 
 for po_file in po/*.po
 do
+    printf "$po_file: "
     msgmerge "$po_file" po/_base.pot -U
     rm -f "$po_file~"
 done
