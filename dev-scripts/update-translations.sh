9a8a21ef6db7a67879b552d41bf01141809c7b62
Naqua Darazaki <n.darazaki@gmail.com>
2024-04-14 15:00:01 +0200

    Improve `dev-scripts/update-translations.sh`

--- dev-scripts/update-translations.sh
+++ dev-scripts/update-translations.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 set -e
-echo '/!\ Make sure that po/_base.pot has been updated with the new translations!' 2>&1
+
+printf "po/_base.pot: "
+xgettext \
+    -f po/POTFILES \
+    -x po/_excluded.pot \
+    -cTR: \
+    --omit-header
+msgmerge -UN po/_base.pot messages.po
+rm -f messages.po 'po/_base.pot~'
 
 for po_file in po/*.po
 do
     printf "$po_file: "
-    msgmerge "$po_file" po/_base.pot -U
+    msgmerge -UN "$po_file" po/_base.pot
     rm -f "$po_file~"
 done
+
+sed -E 's/POT-Creation-Date: [0-9]{4}(-[0-9]{2}){2} [0-9]{2}:[0-9]{2}\+[0-9]{4}/'"POT-Creation-Date: $(date "+%Y-%m-%d %H:%M%z")"'/g' -i po/_base.pot po/*.po
